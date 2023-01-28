d4661171e0278b9a506a60383695c6d98ea69e36
Marlon Couto <marloncouto.dev@gmail.com>
2023-01-28 14:04:12 -0300

    Finaliza setup inicial da página

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        burtons: 'burtons',
+      },
+    },
   },
   plugins: [],
-}
+};
