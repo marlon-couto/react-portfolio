cc01274e42162f78c2a62cd12dea29c2cb06afa2
Marlon Couto <marloncouto.dev@gmail.com>
2023-01-31 13:59:24 -0300

    Adiciona componentes PortfolioHead e MainContent

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import PortfolioHead from '../components/PortfolioHead';
+import MainContent from '../components/MainContent';
+
+export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className={darkMode && 'dark'}>
+      <PortfolioHead />
+      <MainContent darkMode={darkMode} setDarkMode={setDarkMode} />
+    </div>
+  );
+}
